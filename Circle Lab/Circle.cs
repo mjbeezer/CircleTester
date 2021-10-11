b94355205f090ec876316000bc6025b7d847310b
Matt B <mjbeezer11621@gmail.com>
2021-10-11 19:56:38 -0400

    first commit

---
+++ Circle Lab/Circle.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Circle_Lab
+{
+    class Circles
+    {
+        private double _radius;
+        public double Circumference;
+        public double Area;
+
+        public double Radius
+        {
+            get { return _radius; }
+            set { this._radius = value; }
+        }
+        //constructor
+        public Circles(double radius)
+        {
+            this._radius = radius;
+        }
+        //methods
+        public double CalculateCircumference()
+        {
+            return 2 * Math.PI * this._radius;
+        }
+        
+        public string CalculateFormattedCircumference()
+        {
+            double circle = CalculateCircumference();
+            return FormatNumber(circle);
+        }
+
+        public double CalculateArea()
+        {
+            return Math.PI * this._radius * this._radius;
+        }
+
+        public string CalculateFormattedArea()
+        {
+            double area = CalculateArea();
+            return FormatNumber(area);
+        }
+
+        private string FormatNumber(double x)
+        {
+            return Math.Round(x, 2).ToString();
+        }
+    }
+}
+
---
+++ Circle Lab/Program.cs
@@ -0,0 +1,79 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace Circle_Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Circles Tester!");
+            bool runProgram = true;
+            bool keepGoing = true;
+            int counter = 0;
+
+            //create list to count circles
+            List<Circles> Circles = new List<Circles>();
+
+            while (runProgram)
+            {
+
+                //user input and arithmetic
+                Console.WriteLine("Please enter a radius");
+                
+                try
+                {
+                    double radius = double.Parse(Console.ReadLine());
+                    if (radius <= 0)
+                    {
+                        Console.WriteLine("Please enter a number above 0.");
+                        continue;
+                    }
+                    else
+                    {
+                        //call methods for calculations
+                        Circles circle = new Circles(radius);
+                        counter++;
+                        //add count to list
+                        Circles.Add(circle);
+
+                        //display circunference and area
+                        Console.WriteLine($"Area is: {circle.CalculateFormattedArea()}");
+                        Console.WriteLine($"Circumference is: {circle.CalculateFormattedCircumference()}");
+                    }
+                }
+                catch(FormatException e)
+                {
+                    Console.WriteLine(e.Message);
+                    continue;
+                }
+
+                    while (keepGoing)
+                    {
+                        Console.WriteLine("Would you like to make another circle?");
+                        string choice = Console.ReadLine().ToLower();
+                        if (choice == "y")
+                        {
+                            keepGoing = true;
+                            break;
+                        }
+                        else if (choice == "n")
+                        {
+                            keepGoing = false;
+                            runProgram = false;
+                            Console.WriteLine($"You have calculated {Circles.Count} circles.");
+                            Console.WriteLine("Goodbye");
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Thats not an answer!");
+                        }
+                    }
+                
+            }
+        }
+    }
+}
+
